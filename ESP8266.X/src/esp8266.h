c9585a3500846c17d0bc1ab598de4362f5aa052c
Camil Staps <info@camilstaps.nl>
2015-02-27 19:21:43 +0100

    Basic framework

---
+++ ESP8266.X/src/esp8266.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   esp8266.h
+ * Author: Camil Staps <info@camilstaps.nl>
+ */
+
+#ifndef ESP8266_H
+#define	ESP8266_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* ESP8266_H */
+
