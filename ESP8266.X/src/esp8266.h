82b7a3d3d967193959d8771329fb5109511b3087
Camil Staps <info@camilstaps.nl>
2015-02-28 18:35:08 +0100

    Added some functions

--- ESP8266.X/src/esp8266.h
+++ ESP8266.X/src/esp8266.h
@@ -3,6 +3,8 @@
  * Author: Camil Staps <info@camilstaps.nl>
  */
 
+#include <stdbool.h>
+
 #ifndef ESP8266_H
 #define	ESP8266_H
 
@@ -16,16 +18,20 @@ extern "C" {
 #define ESP8266_TCP 1
 #define ESP8266_UDP 0
 
-void esp8266_waitDone(void);        // Wait until the ESP has finished the last command
-bit esp8266_isStarted(void);       // Check if the module is started (AT)
+#define ESP8266_OK 1
+#define ESP8266_READY 2
+#define ESP8266_FAIL 3
+#define ESP8266_NOCHANGE 4
+
+bit esp8266_isStarted(void);        // Check if the module is started (AT)
 void esp8266_restart(void);         // Restart module (AT+RST)
-void esp8266_echoCmds(bit);        // Enabled/disable command echoing (ATE)
+void esp8266_echoCmds(bool);        // Enabled/disable command echoing (ATE)
 
 // WIFI Mode (station/softAP/station+softAP) (AT+CWMODE)
 void esp8266_mode(unsigned char);
 
 // Connect to AP (AT+CWJAP)
-bit esp8266_connect(unsigned char*, unsigned char*);
+unsigned char esp8266_connect(unsigned char*, unsigned char*);
 
 // Disconnect from AP (AT+CWQAP)
 void esp8266_disconnect(void);
@@ -40,12 +46,21 @@ bit esp8266_start(bit protocol, char* ip, unsigned char port);
 bit esp8266_send(unsigned char*);
 
 // User implementation: in and output
-void esp8266_putch(unsigned char);
-unsigned char esp8266_getch(void);
+void _esp8266_putch(unsigned char);
+unsigned char _esp8266_getch(void);
+
+// Internal functions
+// Print a string
+void _esp8266_print(unsigned const char *);
+// Wait until the ESP has sent 'OK'
+inline void _esp8266_waitOK(void);
+// Wait until the ESP has sent 'ready'
+inline void _esp8266_waitReady(void);
+
+inline unsigned char _esp8266_waitResponse(void);
 
 #ifdef	__cplusplus
 }
 #endif
 
 #endif	/* ESP8266_H */
-
